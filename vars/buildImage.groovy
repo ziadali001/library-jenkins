32891b160713dc9d6a4bab6c9aee174c87fb1368
ziad <zma.cee@gmail.com>
2023-05-04 19:21:52 +0300

    initil commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call(String imageName) {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t $imageName ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push $imageName"
+    }
+}
